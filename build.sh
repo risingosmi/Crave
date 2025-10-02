7e9eb7a1b34801ff160ee2b6ca2d65ceb3edc306
AbdoXBuilder <myitworkingacc@gmail.com>
2025-10-02 12:28:15 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Clean old repo/local_manifests and device/vendor/kernel/hardware trees
+echo ">>> Cleaning old manifests and device/vendor/kernel/hardware directories\n\n\n"
+rm -rf .repo/local_manifests/
+echo ">>> Clear device\n\n"
+rm -rf device/xiaomi/miatoll
+echo ">>> Clear vendor\n\n"
+rm -rf vendor/xiaomi/miatoll
+echo ">>> Clear Camera\n\n"
+rm -rf vendor/xiaomi/miuicamera
+echo ">>> Clear kernel/hardware\n\n"
+rm -rf kernel/xiaomi/sm6250
+echo ">>> Clear hardware \n\n"
+rm -rf hardware/xiaomi
+echo ">>> Clear TimeKeep\n\n"
+rm -rf hardware/sony/timekeep
+
+# ROM source repo
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+
+# Device Tree
+git clone https://github.com/risingosmi/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
+
+# Device Vendor
+git clone https://github.com/risingosmi/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
+
+# Device Kernel
+git clone https://github.com/risingosmi/kernel_xiaomi_sm6250.git -b 16 kernel/xiaomi/sm6250
+
+# Device Hardware 
+git clone https://github.com/risingosmi/hardware_xiaomi.git -b 16 hardware/xiaomi
+
+# Device Camera
+git clone https://github.com/risingosmi/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
+
+# LineageOS TimeKeep
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
+
+# Sync
+/opt/crave/resync.sh
+
+# Set up build environment
+source build/envsetup.sh
+
+# Lunch
+riseup lineage_miatoll user
+
+# ----------- Build 1 (normal testkey build) -----------
+echo ">>> Starting normal build (test-keys)\n\n"
+rise b
+echo ">>> Normal build finished\n\n"
+
+# ----------- Generate signing keys -----------
+echo ">>> Generating release signing keys\n\n"
+gk -s
+
+# ----------- Build 2 (signed build) -----------
+echo ">>> Starting signed build (release-keys)\n\n"
+rise sb
+echo ">>> Signed build finished\n\n"
+
+echo ">>> Both builds completed successfully!\n\n"
