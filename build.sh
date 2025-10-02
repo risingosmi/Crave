ed7b041ad38343660a0a8f86e4c41968b2e26eb9
AbdoXBuilder <myitworkingacc@gmail.com>
2025-10-02 12:39:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,27 +16,34 @@ rm -rf hardware/xiaomi
 echo ">>> Clear TimeKeep\n\n"
 rm -rf hardware/sony/timekeep
 
+echo ">>> Now Rom Initialize local repository\n\n"
 # ROM source repo
 repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 
+echo ">>> Now Cloning Device Tree repository\n\n"
 # Device Tree
 git clone https://github.com/risingosmi/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
 
+echo ">>> Now Cloning Vendor Tree repository\n\n"
 # Device Vendor
 git clone https://github.com/risingosmi/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
 
+echo ">>> Now Cloning Kernel Tree repository\n\n"
 # Device Kernel
 git clone https://github.com/risingosmi/kernel_xiaomi_sm6250.git -b 16 kernel/xiaomi/sm6250
 
 # Device Hardware 
 git clone https://github.com/risingosmi/hardware_xiaomi.git -b 16 hardware/xiaomi
 
+echo ">>> Now Cloning Camera Tree repository\n\n"
 # Device Camera
 git clone https://github.com/risingosmi/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
 
+echo ">>> Now Cloning Timekeep Tree repository\n\n"
 # LineageOS TimeKeep
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
 
+echo ">>> Now Rom In Building Setup\n\n"
 # Sync
 /opt/crave/resync.sh
 
