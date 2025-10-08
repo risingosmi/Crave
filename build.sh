7c7bc72fd72b54d103883e2ad162b3fdbb635b92
AbdoXBuilder <myitworkingacc@gmail.com>
2025-10-08 20:15:39 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,9 +19,9 @@ git clone https://github.com/risingosmi/device_xiaomi_miatoll.git -b 16 device/x
 git clone https://github.com/risingosmi/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
 git clone https://github.com/risingosmi/kernel_xiaomi_sm6250.git -b 16 kernel/xiaomi/sm6250
 git clone https://github.com/risingosmi/hardware_xiaomi.git -b 16 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
 git clone https://github.com/risingosmi/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
 git clone https://github.com/risingosmi/vendor-risingos_priv-keys.git -b 16 vendor/lineage-priv/keys
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
 
 # ================================
 # Sync remaining sources
@@ -44,5 +44,5 @@ gk -f
 # Start build
 # ================================
 echo ">>> Starting RisingOS Build"
-riseup miatoll eng
+riseup miatoll userdebug
 rise b
