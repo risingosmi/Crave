ebbb453e0569f77d9020f116119f008b0a195b49
AbdoXBuilder <myitworkingacc@gmail.com>
2025-10-08 11:22:03 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,36 +5,39 @@ rm -rf .repo/local_manifests/
 
 
 #Rom Setup
-echo ">>> Now Rom Initialize local repository"
+echo -e ">>> Now Rom Initialize local repository"
 repo init -u https://github.com/risingosmi/RISINGOS -b 16 --git-lfs
 
-echo ">>> Now Cloning KEY repository"
+echo -e ">>> Now Cloning KEY repository"
 git clone https://github.com/risingosmi/vendor-risingos_priv-keys.git -b 16 vendor/lineage-priv/keys
-echo ">>> Now Cloning Device Tree repository"
+echo -e ">>> Now Cloning Device Tree repository"
 git clone https://github.com/risingosmi/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
-echo ">>> Now Cloning Vendor Tree repository"
+echo -e ">>> Now Cloning Vendor Tree repository"
 git clone https://github.com/risingosmi/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
-echo ">>> Now Cloning Kernel Tree repository"
+echo -e ">>> Now Cloning Kernel Tree repository"
 git clone https://github.com/risingosmi/kernel_xiaomi_sm6250.git -b 16 kernel/xiaomi/sm6250
-echo ">>> Now Cloning Hardware Tree repository"
+echo -e ">>> Now Cloning Hardware Tree repository"
 git clone https://github.com/risingosmi/hardware_xiaomi.git -b 16 hardware/xiaomi
-echo ">>> Now Cloning Timekeep Tree repository"
+echo -e ">>> Now Cloning Timekeep Tree repository"
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
-echo "Cloning Micamera"
+echo -e "Cloning Micamera"
 git clone https://github.com/risingosmi/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
 
-
-echo ">>> Now Rom In Building Setup"
+# Resync
 /opt/crave/resync.sh
 
 # Set up build environment
 source build/envsetup.sh
 
-# Singing The Rom 
+# GMS Core build
+export WITH_GMS=true
+export TARGET_USES_PICO_GAPPS=true
+
+# Signing The Rom 
 gk -f
 
 # Lunch
 riseup miatoll user
 
-# ----------- Build 1 (normal testkey build) -----------
+# Build
 rise b
