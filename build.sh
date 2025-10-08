7582bc38df589abf0fac8617151f12edd387b832
AbdoXBuilder <myitworkingacc@gmail.com>
2025-10-08 11:35:25 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 # Clean old repo/local_manifests
+echo -e ">>> Remove Old Local Repo"
 rm -rf .repo/local_manifests/
 
 
@@ -10,18 +12,27 @@ repo init -u https://github.com/risingosmi/RISINGOS -b 16 --git-lfs
 
 echo -e ">>> Now Cloning KEY repository"
 git clone https://github.com/risingosmi/vendor-risingos_priv-keys.git -b 16 vendor/lineage-priv/keys
+
 echo -e ">>> Now Cloning Device Tree repository"
 git clone https://github.com/risingosmi/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
+
 echo -e ">>> Now Cloning Vendor Tree repository"
 git clone https://github.com/risingosmi/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
+
 echo -e ">>> Now Cloning Kernel Tree repository"
 git clone https://github.com/risingosmi/kernel_xiaomi_sm6250.git -b 16 kernel/xiaomi/sm6250
+
 echo -e ">>> Now Cloning Hardware Tree repository"
 git clone https://github.com/risingosmi/hardware_xiaomi.git -b 16 hardware/xiaomi
+
 echo -e ">>> Now Cloning Timekeep Tree repository"
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
+
 echo -e "Cloning Micamera"
 git clone https://github.com/risingosmi/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
+
+
+
 
 # Resync
 /opt/crave/resync.sh
